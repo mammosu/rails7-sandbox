fde0fa66afeb1811980a1b6a9ead1e9b87503c2c
TakuyaHarayama <harayama.develop@gmail.com>
2021-12-21 22:51:47 +0900

    add loading icon in submitting

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["loading"]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hello World!")
+  }
+
+  disableSubmit() {
+    this.submitButtons().forEach(button => {
+      button.disabled = true
+    })
+    this.loadingTarget.classList.toggle("hidden")
+  }
+
+  submitButtons() {
+    return this.element.querySelectorAll("input[type='submit']")
   }
 }
