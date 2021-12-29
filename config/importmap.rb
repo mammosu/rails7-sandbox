c8238300dd9ac2b5a0f2f98163f4e66411d8ae2a
TakuyaHarayama <harayama.develop@gmail.com>
2021-12-30 02:49:40 +0900

    add activestorage and slider ui

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/activestorage", to: "https://ga.jspm.io/npm:@rails/activestorage@7.0.0/app/assets/javascripts/activestorage.esm.js"
+pin "stimulus-carousel", to: "https://ga.jspm.io/npm:stimulus-carousel@4.0.0/dist/stimulus-carousel.es.js"
+pin "dom7", to: "https://ga.jspm.io/npm:dom7@4.0.2/dom7.esm.js"
+pin "ssr-window", to: "https://ga.jspm.io/npm:ssr-window@4.0.2/ssr-window.esm.js"
+pin "swiper/bundle", to: "https://ga.jspm.io/npm:swiper@7.4.1/swiper-bundle.esm.js"
